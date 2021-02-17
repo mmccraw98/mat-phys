1bc610197bc5b58570922cbb42858de1d5d9a7d4
mmccraw98 <40552161+mmccraw98@users.noreply.github.com>
2021-02-16 19:11:06 -0500

    Add files via upload

--- afm_eom_test.py
+++ afm_eom_test.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 hf.tic()
 
 # time settings
-dt = 2e-10  # s SETTING berkin suggests mHz
+dt = 2e-11  # s SETTING berkin suggests mHz
 tF = 0.2  # s SETTING
 tI = 0  # s
 
@@ -47,12 +47,12 @@ f_ts = np.zeros(time.shape)
 ht = np.zeros(time.shape)
 
 # defining initial conditions of the cantilever
-zt[0] = 5  # nm SETTING
+zt[0] = 1  # nm SETTING
 zb[0] = zt[0]
 
 # creating the material matrices and coefficients
 surface_height = 0
-model_params = {'Ee': 1, 'arms': [{'E': 1, 'T': 1e-5}, {'E': 1, 'T': 1e-4}, {'E': 1, 'T': 1e-3}]}
+model_params = {'Ee': 1e4, 'arms': [{'E': 1e4, 'T': 1e-5}]}
 
 un, qn = maxwell_coeffs(model_params)
 
@@ -61,17 +61,17 @@ u_matrix = np.zeros((2, un.size))
 
 i = 0
 while i < time.size - 1: #@ TODO: make a 'toy' simulation AFM library???? same backend just smaller numbers -> smaller timestep
+                    #@ TODO: make a simulation class that has an assumptions method which prints the assumptions of the simulation
     # calculate surface penetration depth
     ht[i] = (surface_height - zt[i]) * (zt[i] <= surface_height)  # ensured to always be positive, no abs necessary
 
     # calculate the material matrices
     q_matrix[1, 0] = ht[i]**(3 / 2)  # lowest order strain derivative
-    q_matrix[1, 1:] = [(q_matrix[1, j] - q_matrix[0, j]) / dt
-                       for j in range(1, q_matrix.shape[1])]  # higher order strain derivatives
-
+    for j in range(qn.size - 1):  # higher order strain derivatives
+        q_matrix[1, j + 1] = (q_matrix[1, j] - q_matrix[0, j]) / dt
     u_matrix[1, -1] = (alpha * sum(qn * q_matrix[1]) - sum(un[: -1] * u_matrix[0, :-1])) / un[-1]  # highest order stress derivative
-    u_matrix[1, :-1] = [u_matrix[0, -j - 1] + u_matrix[1, -j] * dt
-                        for j in range(1, u_matrix.shape[1])]  # lower order stress derivatives
+    for j in range(2, un.size + 1): # lower order stress derivatives
+        u_matrix[1, -j] = u_matrix[0, -j] + u_matrix[1, -j + 1] * dt
 
     f_ts[i] = u_matrix[1, 0]  # update interaction force with viscoelastic stress
 
@@ -101,3 +101,5 @@ plt.show()
 
 hf.get_mem_use()
 hf.toc()
+
+print(zt - zb)
