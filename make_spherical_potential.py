33db46f0b1417b8c79e2a31e3a1d08fcda5ae2f7
mmccraw98 <40552161+mmccraw98@users.noreply.github.com>
2021-03-05 13:34:55 -0500

    Add files via upload

--- make_spherical_potential.py
+++ make_spherical_potential.py
@@ -1,5 +1,6 @@
 import helperfunctions as hf
 import numpy as np
+from scipy import ndimage
 
 
 # add sphere potential
@@ -15,7 +16,7 @@ bonds = hf.load(r'C:\Users\Windows\PolymerSimulation\lammps_simulations\bonds.xl
 ids = xyz[:, 0].astype(int)  # only IDS
 xy = xyz[:, :3].astype(float)  # ID, X, Y
 
-dr = 0.01
+dr = 0.001
 
 r_sph = 0.5  # radius of the potential surrounding each sphere
 r_sph_dscr = int(r_sph / dr)  # discretized potential radius
@@ -33,35 +34,26 @@ sim_box = np.zeros((int(np.floor((y_max - y_min) / dr)), int(np.floor((x_max - x
 # put the 'image' of the circular potential around each of the sphere's centers
 for sx, sy in np.floor(xy_r).astype(int):
     sim_box[sy - r_sph_dscr: sy + r_sph_dscr + 1, sx - r_sph_dscr: sx + r_sph_dscr + 1] += sph_clone
+sim_box = np.sign(sim_box)  # set all nonzero positive values to 1 to avoid double counting of overlapping potentials
 
-sim_box = np.sign(sim_box)
-
-electrode_separation = int(1 / dr)
-max_high, min_low = int(0.1 * sim_box.shape[0]), int(0.9 * sim_box.shape[0])
-highs, lows = [], []
-for col in sim_box.T:
-    high, low = 0, 0
-    if sum(col) == 0:
-        high = max_high
-        low = min_low
-    else:
-        for i, val in enumerate(col):
-            if val != 0:
-                if high == 0:
-                    high = i
-                if i > low:
-                    low = i
-        if high > max_high:
-            high = max_high
-        if low < min_low:
-            low = min_low
-    col[low + electrode_separation:] = 1
-    col[:high - electrode_separation] = 1
+# form electrodes
+electrode_separation, sharpness = int(1 / dr), 0.8  # required separation distance from the electrode and nearest molecule
+electrode = ndimage.gaussian_filter(np.abs(sim_box - 1), sigma=electrode_separation)  # get 'inverse' image and smooth
+electrode[electrode < sharpness] = 0.0
+electrode[electrode >= sharpness] = 1.0
+electrode[int(0.1 * sim_box.shape[0]): int(0.9 * sim_box.shape[0])] = 0.0  # only consider the lower and upper boundaries for electroding
+sim_box += electrode  # put the electrodes into the sim box
 
 import matplotlib.pyplot as plt
-cmap = plt.cm.gray
-norm = plt.Normalize(sim_box.min(), sim_box.max())
-rgba = cmap(norm(sim_box))
-rgba[range(10), range(10), :3] = 1, 0, 0
-plt.imshow(rgba, interpolation='nearest')
+# cmap = plt.cm.gray
+# norm = plt.Normalize(sim_box.min(), sim_box.max())
+# rgba = cmap(norm(sim_box))
+# rgba[range(10), range(10), :3] = 1, 0, 0
+# plt.imshow(rgba, interpolation='nearest')
+# plt.show()
+# plt.imshow(sim_box)
+# plt.show()
+
+
+plt.imshow(sim_box)
 plt.show()
